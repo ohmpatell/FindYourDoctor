3051e7501719acdfa3bf2f5af30fb6f898a6951b
Ohm Patel <ohm2003.patel@gmail.com>
2025-02-19 23:25:47 -0500

    initialize project with React, Vite, and Express setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <img src="logo.png" alt="doctor" style={{height: "100px", width: "auto"}}/>
+      <h1>Welcome to FindYourDoctor</h1>
+    </>
+  )
+}
+
+export default App
