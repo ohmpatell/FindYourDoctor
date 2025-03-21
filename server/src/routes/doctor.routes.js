50e1aaac1d4e060ffb7dcd945236cf7fa8404f75
wlau83 <workhkmh@gmail.com>
2025-03-20 20:27:43 -0400

    Doctor Detail, and changed the filter function

--- server/src/routes/doctor.routes.js
+++ server/src/routes/doctor.routes.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const Doctor = require('../models/Doctor'); // Import the Doctor model
+const Doctor = require('../models/Doctor'); 
 
 // GET all doctors
-router.get('/doctors', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const doctors = await Doctor.find().populate('clinic'); // Populate clinic details if needed
+    const doctors = await Doctor.find().populate('clinic');
     res.status(200).json(doctors);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching doctors', error });
+  }
+});
+
+// GET single doctor detail
+router.get('/:id', async (req, res) => {
+  try {
+    const doctor = await Doctor.findById(req.params.id).populate('clinic');
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching doctor detail', error });
   }
 });
 
